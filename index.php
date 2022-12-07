6eb5e84c0fb1aed81a4d698ba7e2d4175011cfe6
gab1144 <gabry.rinciari04@gmail.com>
2022-12-07 15:20:16 +0100

    [add] classe e var_dump istanze

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+  include_once './class/Movie.php';
+
+  $movie1 =  new Movie("Ritorno al futuro", 116, "Fantascienza");
+  $movie2 =  new Movie("Forrest Gump ", 142, "Drammatico");
+
+  var_dump($movie1);
+  
+  var_dump($movie2);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  
+</body>
+</html>
